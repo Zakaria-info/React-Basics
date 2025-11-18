ad55350cf19b156c17558e4533843f9c7976abb1
Zakaria-info <info.zakariahossain01@gnail.com>
2025-11-18 14:31:55 +0600

    Added navbar and updated layout

---
+++ src/components/TextForm.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function TextForm(){
+  return (
+    <div>
+        <div className="mb-3">
+            <input className="input-text" id="inputGroup-sizing-sm">Small</input>
+        </div>
+    </div>
+  )
+}
