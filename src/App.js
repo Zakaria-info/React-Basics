3bea857ad3d6715fcef5ae922d29b8d6b11ae8f5
Zakaria-info <info.zakariahossain01@gnail.com>
2025-11-19 17:57:16 +0600

    Added bg image and some other function and logic to my website

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
+import React from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/textForm";
+import bg from "./bg-2.jpg";
 
 function App() {
 
   return (
     <>
       <Navbar title="TextUtils" />
-      <textForm />
+      <div className="container main-background" style={{ backgroundImage: `url(${bg})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+          height: "100vh",
+          width: "100%" }}>
+          <main style={{padding: "1rem"}}>
+            <TextForm heading="Enter the text to analyze below" />
+          </main>
+      </div>
     </>
   );
 }
