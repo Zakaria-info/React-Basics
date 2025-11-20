a7b5914c9a5272dc360c5fabcc8f862059f04891
Zakaria-info <info.zakariahossain01@gnail.com>
2025-11-20 17:57:01 +0600

    Modified all component's and add more logic

--- src/App.js
+++ src/App.js
@@ -9,12 +9,7 @@ function App() {
   return (
     <>
       <Navbar title="TextUtils" />
-      <div className="container main-background" style={{ backgroundImage: `url(${bg})`,
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-          backgroundRepeat: "no-repeat",
-          height: "100vh",
-          width: "100%" }}>
+      <div className="container-fluid main-background" style={{ backgroundImage: `url(${bg})`}}>
           <main style={{padding: "1rem"}}>
             <TextForm heading="Enter the text to analyze below" />
           </main>
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -18,16 +18,16 @@ export default function TextForm(props) {
   const [text, setText] = useState('');
   return (
     <>
-    <div className='container'>
+    <div className='textform-container'>
         <div className="mb-3">
             <h1>{props.heading}</h1>
             <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
         </div>
-        <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
-        <button className="btn btn-primary mx-2" onClick={handleLoClick}>Convert to Lowercase</button>
+        <button className="btn mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
+        <button className="btn mx-2" onClick={handleLoClick}>Convert to Lowercase</button>
     </div>
-    <div className="container my-3">
-      <h1>Text Summary</h1>
+    <div className="text-summary my-3">
+      <h2>Text Summary</h2>
       <p>{text.split(" ").length} words and {text.length} characters</p>
       <p>It takes only {0.008 *text.split(" ").length} Minutes to read if you move your mouth like tortoise.  </p>
       <h2>Preview</h2>
